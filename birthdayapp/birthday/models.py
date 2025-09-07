66dd862fbe5ef14d76c835234617f02ff92faf7c
aryan <aryan.awati@gmail.com>
2025-09-07 16:33:59 -0400

    Created the basic Django framework for the website

--- birthdayapp/birthday/models.py
+++ birthdayapp/birthday/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Birthday(models.Model):
+    email = models.CharField(max_length=100)
+    birthday = models.DateField()
+
+    def __str__(self):
+        return f"{self.name} - {self.birthday}"
--- birthdayapp/birthday/views.py
+++ birthdayapp/birthday/views.py
@@ -1,7 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .forms import BirthdayForm
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, ")
+    return HttpResponse("Hello, world. Welcome to Birthdays!")
+
+def birthday_signup(request):
+    if request.method == "POST":
+        form = BirthdayForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("success")
+    else:
+        form = BirthdayForm()
+    return render(request, "birthday/signup.html", {"form": form})
+
+def success(request):
+    return render(request, 'birthdays/success.html')
+
